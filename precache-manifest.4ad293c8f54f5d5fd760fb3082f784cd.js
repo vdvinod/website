263144186c70bfb09f79198724fb085501f85717
vinod singh rawat <vdvinodrawat@gmail.com>
2020-03-14 18:19:17 +0530

    Updates

---
+++ precache-manifest.4ad293c8f54f5d5fd760fb3082f784cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b87f50c797c63a6046abc7c6cb0c931",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "1f265ca30ba9c2ea2c92",
+    "url": "/website/static/css/main.e067941b.chunk.css"
+  },
+  {
+    "revision": "0f4d6c823240e55c61ff",
+    "url": "/website/static/js/2.7245b878.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/website/static/js/2.7245b878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f265ca30ba9c2ea2c92",
+    "url": "/website/static/js/main.b983f598.chunk.js"
+  },
+  {
+    "revision": "50f7b632da8d827f1feb",
+    "url": "/website/static/js/runtime-main.68a6d9fc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  }
+]);
